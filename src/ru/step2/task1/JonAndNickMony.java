54354c7fd5b336a2f913c0e2f7d554ce0abf5ec5
Panda <ausazhnev@gmail.com>
2025-03-05 19:00:23 +0400

    ADD: Задание №1 второго этапа добавлен class JonAndNickMony

---
+++ src/ru/step2/task1/JonAndNickMony.java
@@ -0,0 +1,12 @@
+package ru.step2.task1;
+
+public class JonAndNickMony {
+    public static void main(String[] args) {
+        int jonMony = 100;
+        int nickMony;
+
+        nickMony = jonMony * 5;
+
+        System.out.println("У Джона и Ника вместе: " + (jonMony + nickMony) + "$");
+    }
+}
