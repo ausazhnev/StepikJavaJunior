cbea1c7216e53850582eef0d4eed436bdfb60735
Panda <ausazhnev@gmail.com>
2025-03-08 09:29:50 +0400

    ADD: Задание №7 второго этапа добавлен class Strings

---
+++ src/ru/step2/task7/Strings.java
@@ -0,0 +1,10 @@
+package ru.step2.task7;
+
+public class Strings {
+    public static void main(String[] args) {
+        String name = "Panda";
+        int age = 41;
+
+        System.out.println("Привет старый " + name + " тебе уже " +age+ " год.");
+    }
+}
