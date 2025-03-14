291a42f4fa7c023cd61cf9db029addd697e303f1
Panda <ausazhnev@gmail.com>
2025-03-14 13:15:32 +0300

    ADD: Добавлен класс WorkerTestDrive для тестирования класса Worker

---
+++ src/ru/step3/task5/WorkerTestDrive.java
@@ -0,0 +1,9 @@
+package ru.step3.task5;
+
+public class WorkerTestDrive {
+    public static void main(String[] args) {
+        Worker worker = new Worker("R2D2", "Штатный робот", 42, false);
+
+        worker.getInfo();
+    }
+}
