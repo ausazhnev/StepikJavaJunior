630b01fbe8a5bfba8dd85b23d108e9e2fdfd2d1c
Panda <ausazhnev@gmail.com>
2025-03-14 11:19:01 +0300

    UPDATE: Добавлен параметр speed и метод run

--- src/ru/step3/task3/Dog.java
+++ src/ru/step3/task3/Dog.java
@@ -4,11 +4,18 @@ public class Dog {
     String name;
     String breed;
     int weight;
+    int speed;
 
     String getInfo(){
         return "Я собака!\n" +
                 "Мое имя: " + name + "\n" +
                 "Порода: " + breed + "\n" +
                 "Возраст: " + weight + " лет.";
+    }
+
+    void run (){
+        for (int i=0; i<speed; i++){
+            System.out.println("Бегу ");
+        }
     }
 }
