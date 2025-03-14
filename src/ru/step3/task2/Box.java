c096f9629e6bb04f09d3dd5d5d6489b532c0a371
Panda <ausazhnev@gmail.com>
2025-03-14 08:25:54 +0300

    ADD: Добавлен класс Box

---
+++ src/ru/step3/task2/Box.java
@@ -0,0 +1,13 @@
+package ru.step3.task2;
+
+public class Box {
+    double length;
+    double width;
+    double height;
+
+    double getVolume() {
+        double volume = length * width * height;
+        return volume;
+
+    }
+}
