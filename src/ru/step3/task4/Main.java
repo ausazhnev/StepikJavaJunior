3c5e636f7f5f18ecdc54cc498eb1aa79f00d152f
Panda <ausazhnev@gmail.com>
2025-03-14 11:56:13 +0300

    ADD: Добавлен класс Main

---
+++ src/ru/step3/task4/Main.java
@@ -0,0 +1,10 @@
+package ru.step3.task4;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle box = new Rectangle();
+        box.setParam(10, 15);
+        System.out.println("Площадь равна: " + box.getSquare());
+
+    }
+}
