a373b15296a130431f55acba5e3e441bf17040e9
Panda <ausazhnev@gmail.com>
2025-03-15 15:54:49 +0300

    UPDATE: Добавлен конструктор создающий объект на основе класса

--- src/ru/step3/task2/Box.java
+++ src/ru/step3/task2/Box.java
@@ -5,12 +5,26 @@ public class Box {
     double width;
     double height;
 
+    Box(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
+    }
+
+    Box() {
+        this(10, 10, 10);
+    }
+
+    Box (Box newBox) {
+        this(newBox.length, newBox.width, newBox.height);
+    }
+
     double getVolume() {
         double volume = length * width * height;
         return volume;
     }
 
-    int compare (Box newbox) {
+    int compare(Box newbox) {
         if (this.getVolume() > newbox.getVolume()) {
             return 1;
         } else if (this.getVolume() < newbox.getVolume()) {
