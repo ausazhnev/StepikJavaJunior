c532988c2dbeaaaae1fa2b0ef4818a00ef723d7e
Panda <ausazhnev@gmail.com>
2025-03-15 11:47:03 +0300

    UPDATE: Добавлен метод сравнения объемов коробок compare

--- src/ru/step3/task2/Box.java
+++ src/ru/step3/task2/Box.java
@@ -8,6 +8,15 @@ public class Box {
     double getVolume() {
         double volume = length * width * height;
         return volume;
+    }
 
+    int compare (Box newbox) {
+        if (this.getVolume() > newbox.getVolume()) {
+            return 1;
+        } else if (this.getVolume() < newbox.getVolume()) {
+            return -1;
+        } else {
+            return 0;
+        }
     }
 }
