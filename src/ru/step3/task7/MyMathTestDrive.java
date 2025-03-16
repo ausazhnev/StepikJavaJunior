ee8a397935e5f945e37b588eddd1414f3b0547f4
Panda <ausazhnev@gmail.com>
2025-03-16 09:30:18 +0300

    ADD: Добавлен класс MyMathTestDrive Проверяющий работу класса MyMath

---
+++ src/ru/step3/task7/MyMathTestDrive.java
@@ -0,0 +1,17 @@
+package ru.step3.task7;
+
+public class MyMathTestDrive {
+    public static void main(String[] args) {
+        System.out.println(MyMath.square(12));
+        System.out.println(MyMath.square(6.3f));
+        System.out.println(MyMath.square(8.8372));
+
+        System.out.println(MyMath.cube(12));
+        System.out.println(MyMath.cube(6.3f));
+        System.out.println(MyMath.cube(8.8372));
+
+        System.out.println(MyMath.circleLength(50));
+
+        System.out.println(MyMath.areaCircle(52.12));
+    }
+}
