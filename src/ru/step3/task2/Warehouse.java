473e2f8a360585d1589f3cc9f4e7669e92761ff8
Panda <ausazhnev@gmail.com>
2025-03-19 15:31:58 +0300

    UPDATE: Обновлен класс Warehouse добавлено тестирование под клааса

--- src/ru/step3/task2/Warehouse.java
+++ src/ru/step3/task2/Warehouse.java
@@ -2,20 +2,23 @@ package ru.step3.task2;
 
 public class Warehouse {
     public static void main(String[] args) {
-        Box box1 = new Box();
+        WeightBox wBox = new WeightBox();
+        wBox.getInfo();
+
+//        Box box1 = new Box();
 
 //        box1.height = 10;
 //        box1.width = 10;
 //        box1.length = 10;
 
-        box1.getInfo();
-
-        Box box2 = box1.copy();
-        box2.getInfo();
-
-        Box box3 = box2.increm();
-
-        box3.getInfo();
+//        box1.getInfo();
+//
+//        Box box2 = box1.copy();
+//        box2.getInfo();
+//
+//        Box box3 = box2.increm();
+//
+//        box3.getInfo();
 
 
 //        box2.height = 5;
