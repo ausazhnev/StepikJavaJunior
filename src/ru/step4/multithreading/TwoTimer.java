bb635b539d30a496a9828f7e46b1d53d1d7a9739
Panda <ausazhnev@gmail.com>
2025-03-21 20:36:49 +0300

    ADD: Класс TwoTimer

---
+++ src/ru/step4/multithreading/TwoTimer.java
@@ -0,0 +1,44 @@
+package ru.step4.multithreading;
+
+public class TwoTimer {
+static boolean isStop = false;
+
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 60; i++) {
+                    if (i == 5) {
+                        isStop = true;
+                    }
+                    System.out.println(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        thread1.start();
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 60; i++) {
+                    if (isStop) {
+                        break;
+                    }
+                    System.out.println(i);
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        });
+        thread2.start();
+    }
+}
+
