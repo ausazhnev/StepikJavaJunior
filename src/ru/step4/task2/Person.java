08a1063bfe15a88be22f47d37461c3e5ad226aeb
Panda <ausazhnev@gmail.com>
2025-03-21 14:22:05 +0300

    ADD: Интерфейс Person

---
+++ src/ru/step4/task2/Person.java
@@ -0,0 +1,17 @@
+package ru.step4.task2;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person() {
+        this.name = "John Doe";
+        this.age = 0;
+    }
+
+}
