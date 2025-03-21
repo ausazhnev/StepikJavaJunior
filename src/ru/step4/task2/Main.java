aa3235906792aa1c514255b71d24cf6735df03fc
Panda <ausazhnev@gmail.com>
2025-03-21 14:24:01 +0300

    ADD: Класс Main для тестирования класса Person

---
+++ src/ru/step4/task2/Main.java
@@ -0,0 +1,9 @@
+package ru.step4.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+        System.out.println(person.toString());
+        System.out.println(person);
+    }
+}
